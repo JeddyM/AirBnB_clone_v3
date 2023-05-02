578434cf76e4357c5485c2a70abb4af4d8379556
Jeddy Mweru <jeddymweru@gmail.com>
2023-05-02 22:17:39 +0000

    Fixing index and adding stat endpoint

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,14 +1,31 @@
 #!/usr/bin/python3
 """Module to route status"""
 from api.v1.views import app_views
-from flask import jsonify
+from flask import Flask, Blueprint, jsonify
+from models import storage
 
-app_views.route('/status')
+classes = {
+    "amenities": "Amenity",
+    "cities": "City",
+    "places": "Place",
+    "reviews": "Review",
+    "states": "State",
+    "users": "User"
+    }
 
 
+@app_views.route('/status')
 def status():
     """Returns a JSON: "status": "OK"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """Endpoint that retrieves the number of each objects by type"""
+    dictionary = {}
+    dictionary = {key: storage.count(value) for key, value in classes.items()}
+    return jsonify(dictionary)
 
 
 if __name__ == "__main__":
